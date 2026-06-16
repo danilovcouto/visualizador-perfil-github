ac2621f10e0c3ceb988ac631e1a9df56c3fa2708
Danilo Couto <danilovcouto100@gmail.com>
2026-06-15 23:23:50 -0300

    feat: adiciona lista com 10 últimos repositórios

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,6 +1,7 @@
-export async function fetchGithubUser(username) {
-    const BASE_URL = 'https://api.github.com';
-    const response = await fetch(`${BASE_URL}/users/${username}`, {
+const BASE_URL = 'https://api.github.com';
+
+export async function fetchGithubUser(userName) {
+    const response = await fetch(`${BASE_URL}/users/${userName}`, {
         headers: {
             'Accept': 'application/vnd.github.v3+json',
             'X-GitHub-Api-Version': '2026-03-10'
@@ -8,6 +9,14 @@ export async function fetchGithubUser(username) {
     });
     if (!response.ok) {
         throw new Error('Usuário nao encontrado.');
+    }
+    return await response.json();
+}
+
+export async function fetchGithubUserRepos(userName) {
+    const response = await fetch(`${BASE_URL}/users/${userName}/repos?per_page=10&sort=created`);
+    if (!response.ok) {
+        throw new Error('Repositorios não encontrados.');
     }
     return await response.json();
 }
